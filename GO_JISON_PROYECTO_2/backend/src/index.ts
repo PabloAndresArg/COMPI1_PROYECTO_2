942823c3a6adf38c8ecd11b96124f5d56926d6be
PabloAndresArg <PabloAndresArg@gmail.com>
2020-05-21 18:47:42 -0600

    .

--- GO_JISON_PROYECTO_2/backend/src/index.ts
+++ GO_JISON_PROYECTO_2/backend/src/index.ts
@@ -10,7 +10,7 @@ import { fstat } from 'fs';
 import { json } from 'body-parser';
 
 
-import {GraficaArbolAts} from './ManejoErrores/GraficaArbolAts'; 
+import { GraficaArbolAts } from './ManejoErrores/GraficaArbolAts';
 
 
 var bodyParser = require("body-parser");
@@ -21,7 +21,7 @@ const app = express();
 const port = 3000;
 
 app.use(cors());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 
@@ -57,10 +57,10 @@ app.post('/analizar', (req, res) => {
   }
   //const tree = MyParser_300445.parse(entrada);
   const tree = parser.parse(entrada);
-  console.log("entra al arbol:"+ entrada);
+  console.log("entra al arbol:" + entrada);
   const tabla = new Table(null);
- 
-  var contador_de_sentencias = 0 ; 
+
+  var contador_de_sentencias = 0;
   tree.instructions.map((m: any) => {
     const res = m.execute(tabla, tree);
     contador_de_sentencias++;
@@ -74,12 +74,12 @@ app.post('/analizar', (req, res) => {
       const error = new Exception('Semantico',
         `Sentencia continue fuera de un ciclo`,
         res.line, res.column);
-      tree.excepciones.push(error); 
+      tree.excepciones.push(error);
       tree.console.push(error.toString());
     }
-    console.log("# de sentencias: "+contador_de_sentencias);
+    console.log("# de sentencias: " + contador_de_sentencias);
   });
-  
+
   res.render('views/index', {
     entrada,
     consola: tree.console,
@@ -123,38 +123,30 @@ app.post('/analizarYO', (req, res) => {
   if (!entrada) {
     return res.redirect('/');
   }
- 
- const tree = MyParser_300445.parse(entrada); 
- // console.log("entra al arbol:"+ entrada);
+  const tree = MyParser_300445.parse(entrada);
   const tabla = new Table(null); // EN ESTE CASO mando null porque previamente no tengo ninguna TABLA 
   console.log("-------------INICIA EL ARBOL----------------");
   console.log(tree);
   console.log("------------------- FIN -------------------");
- /*
+  /*convierte el arbol en JSON
+   try {
+   var json = JSON.stringify(tree,null ,2);
+  // console.log(json);
+  } catch (error) {
+    console.log("ERROR..!!!!!!!!!  AL PARSEAR A JISON ");
+  
+  }
+  */
   try {
-  var json = JSON.stringify(tree,null ,2);
- // console.log(json);
- } catch (error) {
-   console.log("ERROR..!!!!!!!!!  AL PARSEAR A JISON ");
- 
- }
- */
+    tree.instructions.map((m: any) => {
+      //console.log(m);
+      const res = m.execute(tabla, tree);
 
-
-try{
-  tree.instructions.map((m: any) => {
-    //console.log(m);
-    const res = m.execute(tabla, tree);
-
-  });
-}catch(error){
-  console.log("ERRORES EN LA ENTRADA  en ejecucion del ATS");
-  console.log(Errores.geterror());
-}
-
-
-
- 
+    });
+  } catch (error) {
+    console.log("ERRORES EN LA ENTRADA  en ejecucion del ATS");
+    console.log(Errores.geterror());
+  }
   res.render('views/index', {
     entrada,
     consola: tree.console,
@@ -165,63 +157,72 @@ try{
 
 
 
-app.post('/comunicar/', function (req, res) {
+
+
+
+
+/*
+
+
+                               PETICIONES QUE VAN HACIA EL FRONTED 
+
+*/ 
+
+
+
+
+
+
+app.post('/errores/', function (req, res) { // SOLO ES NECESARIO UNA ENTRADA DE TEXTO 
   Errores.clear();// limpiamos la lista 
-  var entrada1=req.body.text1;
-  var entrada2 = req.body.text2;
-  const tree = MyParser_300445.parse(entrada1); 
- // console.log("entra al arbol:"+ entrada);
-  const tabla = new Table(null); 
-
-  try{
-    tree.instructions.map((m: any) => {
-      //console.log(m);
-      const res = m.execute(tabla, tree);
-  
-    });
-  }catch(error){
-    console.log("ERRORES EN LA ENTRADA  en ejecucion del ATS");
+  var entrada1 = req.body.text1;
+  const tree = MyParser_300445.parse(entrada1);
+  const tabla = new Table(null);
+    
+  if(Errores.hay_errores()){
     console.log(Errores.geterror());
-     res.send(Errores.geterror());
+    res.send(Errores.geterror());
+  }else{
+    console.log("ENTRADA OK ");
+    res.send("ENTRADA LIBRE DE ERRORES");
   }
-  
-
 
 });
 
 
-app.post('/ats/', function (req, res) {
+app.post('/ats/', function (req, res) { // PARA ESTA FUNCION SOLO ES NECESARIA UNA ENTRADA DE TEXTO 
   GraficaArbolAts.clear();
   Errores.clear();
-  var entrada1=req.body.text1;
-  //var entrada2 = req.body.text2;
-  const tree = MyParser_300445.parse(entrada1); 
-  const tabla = new Table(null); 
-  //GraficaArbolAts.initHtml(); 
-  if(Errores.hay_errores()){
-  // SI HAY ERRORES DEBERIA DE DEVOLVERLOS 
-    res.send("LA ENTRADA POSEEE ERRORES");
-  }else{
-  GraficaArbolAts.add("<ul>\n");
-   GraficaArbolAts.add("<li data-jstree='{ \"opened\" : true }'>Raiz\n");
-   GraficaArbolAts.add("<ul>\n");
-    try{
+  var entrada1 = req.body.text1;
+  const tree = MyParser_300445.parse(entrada1);
+  const tabla = new Table(null);
+  if (Errores.hay_errores()) {
+    res.send("LA ENTRADA POSEEE ERRORES , NO SE PUEDE GENERAR EL REPORTE");
+  } else {
+    GraficaArbolAts.add("<ul>\n");
+    GraficaArbolAts.add("<li data-jstree='{ \"opened\" : true }'>Raiz\n");
+    GraficaArbolAts.add("<ul>\n");
+    try {
       tree.instructions.map((m: any) => {
-        console.log(m);
-        const res = m.execute(tabla, tree);
-    
+      const res = m.execute(tabla, tree);
       });
-    }catch(error){
-      console.log("ERRORES EN LA ENTRADA  en ejecucion del ATS");
+    } catch (error) {
+      console.log("ERROR al utilizar el metodo abstracto EXECUTE del ATS");
     }
-  
+
     /*     COMIENZO A RECORRER EL ARBOL PARA ELLO SE VALIDO QUE NO VINIERA CON ERRORES */
-    GraficaArbolAts.add("</ul>\n"); 
+
+    GraficaArbolAts.add("</ul>\n");
     GraficaArbolAts.add("</li>\n");
-    GraficaArbolAts.add("</ul>\n"); 
-    console.log(GraficaArbolAts.cadena);
-    //GraficaArbolAts.initHtml();  
-    res.send(GraficaArbolAts.cadena);
+    GraficaArbolAts.add("</ul>\n");
+
+
+    // pueden haber errores semanticos 
+    if (Errores.hay_errores()) {
+      res.send("LA ENTRADA POSEEE ERRORES , NO SE PUEDE GENERAR EL REPORTE");
+    } else {
+      res.send(GraficaArbolAts.cadena);
+    }
 
   }
 
